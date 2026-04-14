27c4af0be93c80f98a9f22577eb0c3af8af2d161
KyyroxxX <kyrox3333@gmail.com>
2026-04-14 03:47:59 +0200

    fix: correct typo in supabase project url

--- main.js
+++ main.js
@@ -271,7 +271,7 @@ document.addEventListener("DOMContentLoaded", () => {
     // Supabase Backend System
     // ---------------------------------
     // CONFIGURATION
-    const SUPABASE_URL = 'https://qkeiajsyynvpybctxxuv.supabase.co'; 
+    const SUPABASE_URL = 'https://qkeiajxyynvpybctxxuv.supabase.co'; 
     const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFrZWlhanh5eW52cHliY3R4eHV2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYxMjgxODQsImV4cCI6MjA5MTcwNDE4NH0.JTeODgp_ho_XamO-2oR1h0HT-Sv-v9Fe2vpn4KFgOpE';
 
     let supabase = null;
